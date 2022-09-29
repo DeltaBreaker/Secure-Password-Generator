d5929df0ed066eae3ea01df78932521184d97542
willc <willcrain1@gmail.com>
2022-09-28 22:05:41 -0400

    Created vars to hold character options

--- res/script.js
+++ res/script.js
@@ -1,17 +1,15 @@
-// Assignment code here
+var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+var numericCharacters = "0123456789";
+var specialCharacters = "\"!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
