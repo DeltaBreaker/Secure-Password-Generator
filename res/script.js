1c936bffaca2b7efad71f1ac87581111ca2442c2
willc <willcrain1@gmail.com>
2022-09-28 22:07:24 -0400

    Created generatePassword and added character selection prompts

--- res/script.js
+++ res/script.js
@@ -5,11 +5,31 @@ var specialCharacters = "\"!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var validCharacters = "";
+
+  if(confirm("Do you want to include uppercase characters?")) {
+    validCharacters += uppercaseCharacters;
+  }
+  if(confirm("Do you want to include lowercase characters?")) {
+    validCharacters += lowercaseCharacters;
+  }
+  if(confirm("Do you want to include numeric characters?")) {
+    validCharacters += numericCharacters;
+  }
+  if(confirm("Do you want to include special characters?")) {
+    validCharacters += specialCharacters;
+  }
+
+  return "";
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
 
 generateBtn.addEventListener("click", writePassword);
